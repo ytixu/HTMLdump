d8c0903526f0874a2c554b5ea8d53785b0f4f87d
ytixu <yi.t.xu@mail.mcgill.ca>
2015-01-14 19:29:26 -0500

    filling

--- mainPage/new/cover.js
+++ mainPage/new/cover.js
@@ -5,16 +5,44 @@ function createStuff(type, text){
 }
 
 function loadPage1(){
-	var name = createStuff("h1", "Yi Tian ");
-	var lastname = createStuff("span", "Xu");
-	name.appendChild(lastname);
-	$("#name").append(name);
-	$("#name").append(createStuff("h2", ""));
-	$("#pic").append("<img id='imagePic' src='images/pic.png'/>");
 	$("#imagePic").animate({"width": "+=60%"}, "slow");
 	$("#titleTable").animate({"margin-left": "-=22%"}, "slow");
 }
 
 function loadPage2(){
+	$("#text2").slideDown( "slow" );
+}
 
+
+function loadPage3(){
+
+}
+
+
+function loadPage4(){
+
+}
+
+
+function loadPage5(){
+	setTimeout(function(){
+		$("#text5").slideDown( "slow" );
+		setTimeout(function(){
+			$("#text55").slideDown( "slow" );
+		}, 500);
+	}, 1000);
+}
+
+
+function loadPage6(){
+	$("#text6").slideDown( "slow" );
+	pos = true;
+	setTimeout(function(){
+		$('#sliderV').trigger('click');
+	}, 1000);
+}
+
+
+function loadPage7(){
+    $( "#text7" ).slideDown( "slow" );
 }
