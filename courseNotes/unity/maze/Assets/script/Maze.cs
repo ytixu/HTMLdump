c1d559c3b66ebca461c4bea37cd425439fcfefb3
ytixu <yi.t.xu@mail.mcgill.com>
2015-01-23 15:42:06 -0500

    more code

--- courseNotes/unity/maze/Assets/script/Maze.cs
+++ courseNotes/unity/maze/Assets/script/Maze.cs
@@ -5,6 +5,7 @@ public class Maze : MonoBehaviour {
 	public int width, depth;
 	public MazeCell aCell;
 	private MazeCell[,] grid;
+	private static int FloorHeight = -20;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +17,27 @@ public class Maze : MonoBehaviour {
 	
 	}
 
+	private struct MazeVector2{
+		public int x;
+		public int z;
+		public MazeVector2(int a, int b){
+			x = a;
+			z = b;
+		}
+	}
+
+	private MazeVector2 randomCoordinate(){
+		return new MazeVector2 (Random.Range (0, width), Random.Range (0, depth));
+	}
+
+	public Vector3 convertToVector3(int x, int z){
+		return new Vector3 (x * aCell.x, FloorHeight, z * aCell.z);
+	}
+
+	private Vector3 convertToVector3(MazeVector2 v){
+		return convertToVector3 (v.x, v.z);
+	}
+
 	public void initializeMazeCell(){
 		grid = new MazeCell[width, depth];
 		for (int i=0; i<depth; i++){
@@ -24,7 +46,7 @@ public class Maze : MonoBehaviour {
 				grid[i,j] = newCell;
 				newCell.name = "cell"+i+"-"+j;
 				newCell.transform.parent = transform;
-				newCell.transform.localPosition = new Vector3(i*newCell.width, -20f, j*newCell.depth);
+				newCell.transform.localPosition = convertToVector3(i,j);
 			}
 		}
 	}
--- courseNotes/unity/maze/Assets/script/MazeCell.cs
+++ courseNotes/unity/maze/Assets/script/MazeCell.cs
@@ -2,5 +2,5 @@
 using System.Collections;
 
 public class MazeCell : MonoBehaviour {
-	public int width, depth;
+	public int x, z;
 }
