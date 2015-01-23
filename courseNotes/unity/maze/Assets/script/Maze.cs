8812086479a8d731c4130a1dd7d101d2e9b6f732
ytixu <yi.t.xu@mail.mcgill.ca>
2015-01-23 00:10:35 -0500

    game class project

---
+++ courseNotes/unity/maze/Assets/script/Maze.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Maze : MonoBehaviour {
+	public int width, depth;
+	public MazeCell aCell;
+	private MazeCell[,] grid;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void initializeMazeCell(){
+		grid = new MazeCell[width, depth];
+		for (int i=0; i<depth; i++){
+			for (int j=0; j<width; j++){
+				MazeCell newCell = Instantiate(aCell) as MazeCell;
+				grid[i,j] = newCell;
+				newCell.name = "cell"+i+"-"+j;
+				newCell.transform.parent = transform;
+				newCell.transform.localPosition = new Vector3(i*newCell.width, -20f, j*newCell.depth);
+			}
+		}
+	}
+}
+
---
+++ courseNotes/unity/maze/Assets/script/MazeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MazeGenerator : MonoBehaviour {
+	public Maze aMaze;
+	private Maze generatedMaze;
+
+	// Use this for initialization
+	void Start () {
+		CreateMaze ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			Restart();
+		}
+	}
+
+	public void Restart(){
+		Destroy (generatedMaze.gameObject);
+		CreateMaze ();
+	}
+
+	public void CreateMaze (){
+		generatedMaze = Instantiate (aMaze) as Maze;
+		generatedMaze.initializeMazeCell ();
+	}
+}
