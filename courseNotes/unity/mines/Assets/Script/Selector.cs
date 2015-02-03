ce673d1ce0ebe07df1c6be609094da6d434edfad
ytixu <yi.t.xu@mail.mcgill.ca>
2015-02-03 00:23:58 -0500

    select

---
+++ courseNotes/unity/mines/Assets/Script/Selector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void showInCubi(Cubi c){
+		transform.parent = c.transform;
+		renderer.enabled = true;
+	}
+
+	public void hide(){
+		renderer.enabled = false;
+	}
+}
