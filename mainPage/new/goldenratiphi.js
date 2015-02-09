86ae0fc5667531d7be1d83e6c7d89c432bcdda5d
ytixu <yi.t.xu@mail.mcgill.ca>
2015-02-09 03:03:59 -0500

    tiles

--- mainPage/new/goldenratiphi.js
+++ mainPage/new/goldenratiphi.js
@@ -7,6 +7,7 @@ var gr = 1.61803398875;
 var flcol = [100,35,0];
 var overlaycol = [200,200,200];
 var recN = 0;
+var numP = 5; // 89
 
 function drawPetal(n, size){
     ctx.beginPath();
@@ -18,7 +19,7 @@ function drawPetal(n, size){
 }
 
 function drawFlower(c){
-	if (recN>78) return;
+	if (recN>numP) return;
 	ctx.globalAlpha = 0.1
 	ctx.fillStyle = "rgb("+Math.min(c[0]+recN*2,225).toString()+
 					","+Math.min(c[1]+recN*2,225).toString()+
@@ -27,7 +28,7 @@ function drawFlower(c){
 	if (s==0) return;
 	drawPetal(recN,s);
 	recN+=1;
-	if (recN == 78) showThree();
+	if (recN == numP) showThree();
 	// setTimeout(function(){ drawFlower(c); }, 110);
 }
 
@@ -40,6 +41,78 @@ function getCanvas(){
 }
 
 var scrollVal = false;
+
+window.addEventListener ("mousewheel", function (event) {
+	// console.log(scrollVal);
+	if (event.wheelDelta > 0 || scrollVal > 0) return;
+	if (scrollVal == 0){
+		scrollVal = true;
+		$( "#page1" ).animate({
+	   		height: "-=100%"
+	 	}, 500);
+	  	$( "#title" ).animate({
+	   		left: "34%"
+	 	}, 500);
+	 	$( "#title" ).animate({
+	   		left: "-=3%"
+		}, 500);//4000);
+	}else if (scrollVal < -200){
+		drawFlower(flcol);
+	}
+	scrollVal += event.wheelDelta;
+}, false);
+
+window.onload = function(){
+	getCanvas();
+}
+
+
+//////////////////////////////
+
+
+var divs = [];
+var spacing = 10;
+// left, top
+var pos = [parseInt($(window).width()/gr), 60+parseInt($(window).height()*0.05)]
+
+function createDivs(){
+	var h = parseInt($(window).height()*0.8);
+	$("body").append("<div id=\'c0\'></div>");
+	divs.push($("#c0"));
+	h -= spacing/2;
+	$("#c0").css({"width":h.toString(), 
+				"height":h.toString(),
+				"position":"absolute",
+				"left":pos[0]-Math.round(h/2)+"px",
+				"top":pos[1]+"px"});
+	var left = parseInt($("#c0").css("left"));
+	var top = parseInt($("#c0").css("top"));
+	for (var i=1; i<5; i++){
+		console.log(left, top);
+		if (i == 2) top += h+spacing+spacing/2;
+		if (i == 3){
+			 left += h+spacing+Math.round(spacing/3);
+			 top += h+Math.round(spacing/3);
+		}
+		if (i == 4) left += h+spacing/2;
+		h = Math.round(h/gr);
+		if (i == 1) left -= h+spacing;
+		if (i == 4){
+			top -= h+spacing/2;
+			left -= h;
+		}
+		if (i == 3) top -= h;
+		h -= spacing/2;
+		divs.push(document.createElement("div"));
+		$("body").append("<div id=\'c"+i+"\'></div>");
+		$("#c"+i).css({"width":h.toString(), 
+				"height":h.toString(),
+				"position":"absolute",
+				"left":left+"px",
+				"top":top+"px"});
+	}
+}
+
 
 function showThree(){
   	$( "#title" ).fadeOut( "fast", function(){
@@ -55,30 +128,7 @@ function showThree(){
 			   	height: "100%",
 			}, 500);	
 			$( "#title" ).fadeIn( "slow");
+			createDivs();
 		});
   	});
-}
-
-window.addEventListener ("mousewheel", function (event) {
-	console.log(scrollVal);
-	if (event.wheelDelta > 0 || scrollVal > 0) return;
-	if (scrollVal == 0){
-		scrollVal = true;
-		$( "#page1" ).animate({
-	   		height: "-=100%"
-	 	}, 500);
-	  	$( "#title" ).animate({
-	   		left: "34%"
-	 	}, 500);
-	 	$( "#title" ).animate({
-	   		left: "-=3%"
-		}, 4000);
-	}else if (scrollVal < -200){
-		drawFlower(flcol);
-	}
-	scrollVal += event.wheelDelta;
-}, false);
-
-window.onload = function(){
-	getCanvas();
 }
