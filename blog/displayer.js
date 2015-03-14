78fe70c75d16eb729be48c646e39437f6f880ecc
ytixu <yi.t.xu@mail.mcgill.ca>
2015-03-13 22:12:40 -0400

    try

---
+++ blog/displayer.js
@@ -0,0 +1,11 @@
+function httpGet(theUrl)
+{
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false );
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+console.log(httpGet("archive/1.html"));
