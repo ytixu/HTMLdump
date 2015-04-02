8275545cf803cdf4f7b1815e35c30a966596b8eb
yitian <yi.t.xu@mail.mcgill.ca>
2015-04-02 15:50:18 -0400

    n

--- hacknights/query.js
+++ hacknights/query.js
@@ -1,5 +1,5 @@
 //// get location of user
-var location = "montreal";
+var loc = "montreal";
 
 //// set sunrise and sunset
 
@@ -20,16 +20,18 @@ function toNums(arr){
 }
 
 function getSunRiseSunset(str){
-	$.getJSON(mapapi(str)).done(function(data){
+	console.log(str);
+	var res = $.getJSON(mapapi(str)).done(function(data){
 		var coordinates = data.features[0].geometry.coordinates;	
+		console.log(data);
 		$.ajax({
 			dataType: "jsonp",
 			url: sunapi(coordinates[1], coordinates[0]),
 			success: function(data){
 				sunrise = parseTime(data.results.sunrise);
 				sunset = parseTime(data.results.sunset);
-				setTimeCount();
 				save();
+				setTimeCount();
 			}
 		});
 	});
@@ -52,16 +54,109 @@ function save(){
 function load(){
 	sunrisetemp = localStorage.getItem('sunrise').split(",");
 	if (sunrisetemp[0].indexOf("null") > -1){
-		getSunRiseSunset(location);
+		getSunRiseSunset(loc);
 	}else{
 		sunset = toNums(localStorage.getItem('sunset').split(","));
 		sunrise = toNums(sunrisetemp);
+		setTimeCount();
 	}
 }
 
+///// update time
+
+window.requestAnimFrame = (function(){
+  return function( callback ){
+    window.setTimeout(callback, 1000);
+  };
+})();
+
+var sunriseDate;
+var sunsetDate;
+
 function setTimeCount(){
-	console.log(sunrise, sunset);
+	var today = new Date();
+	sunriseDate = new Date(today.getFullYear(), 
+						   today.getMonth(),
+						   today.getDate(),
+						   sunrise[0],
+						   sunrise[1],
+						   sunrise[2]);
+	sunsetDate = new Date(today.getFullYear(), 
+						   today.getMonth(),
+						   today.getDate(),
+						   sunset[0],
+						   sunset[1],
+						   sunset[2]);
+	console.log(today.toDateString(), sunsetDate.toDateString(), sunriseDate.toDateString());
+	count();
+}
+
+function count(){
+	var today = new Date();
+	if (sunsetDate.getTime() < today.getTime()){
+		sunriseDate.setDate(sunriseDate.getDate()+1);
+	}
+	if (today.getTime() >= sunriseDate.getTime() && today.getTime() < sunsetDate.getTime()){
+		$('#timeTitle').html('Starts in');
+		next = sunset;
+	}else{
+		$('#timeTitle').html('Ends in');
+		next = sunrise;
+	}
+	var carry = 0;
+	var seconds = next[2] - today.getSeconds();
+	if (seconds < 0){
+		seconds = 60 + seconds;
+		carry = -1;
+	}
+	var s = seconds.toString();
+	if (s.length < 2){
+		s = "0"+s;
+	}
+	var minutes = next[1] - today.getMinutes() + carry;
+	if (minutes < 0){
+		minutes = 60 + minutes;
+		carry = -1;
+	}else{
+		carry = 0;
+	}
+	var m = minutes.toString();
+	if (m.length < 2){
+		m = "0"+m;
+	}
+	var hours = next[0] - today.getHours() + carry;
+	var h = hours.toString();
+	if (h.length < 2){
+		h = "0"+h;
+	}
+	$('#timeCountDown').html(h+":"+m+":"+s);
+	requestAnimFrame(count)
+}
+
+function displayName(){
+	$("#cityName").html(" " + loc.split(" ").map(function(x){
+		return x.charAt(0).toUpperCase() + x.slice(1);
+	}));
+}
+
+function setLocationName(){
+	$("#locationSubmit").click(updateLocationName);
+	$("#inputCityBlock").hide();
+	displayName();
+	$("#cityName").click(function(){
+		$("#cityName").html('');
+		$("#inputCityBlock").fadeIn("fast");
+	});
+}
+
+function updateLocationName(){
+	var val = $("#inputCity").val();
+	if (val.length > 0){
+		getSunRiseSunset(val);
+	}
+	$("#inputCity").val("");
+	$("#inputCityBlock").fadeOut("fast",displayName);
 }
 
 load();
-
+setLocationName();
