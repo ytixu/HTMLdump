a643cc1d7bef7a56f1403099e320f34cb7490fc4
yitian <yi.t.xu@mail.mcgill.ca>
2015-04-03 16:09:01 -0400

    ahhhhh doomed for graphics assignmet

--- hacknights/query.js
+++ hacknights/query.js
@@ -257,5 +257,41 @@ function stopSpinning(){
 	spinner.stop();
 }
 
+
+/////////////// background image
+
+function getImage(){
+  $.ajax({
+        url: 'http://noapi.dorparasti.ir/api/scraps/e9baeceb-f353-4703-a84d-c9e3107bd90f',
+        cache: false,
+        dataType: 'json',
+        success: function (data) {
+            $("body").css({
+              "background": "url(http://bing.com" + data.Paths[0] + ")",
+              "background-size": "cover",
+              "background-position": "center",
+              "background-repeat": "no-repeat",
+              "background-attachment": "fixed",
+              // "background-blend-mode": "color"
+            });
+            $(".siteTitle").css({
+              // "background": "url(http://bing.com" + data.Paths[0] + ")",
+              "color": "rgba(225,225,225,0.3)",
+              
+              "background": "-webkit-linear-gradient(transparent, transparent), url(http://bing.com" + data.Paths[0] + ") transparent",
+              "background-image": "-o-linear-gradient(transparent, transparent)",
+              "background-size": "cover",
+              "background-position": "center",
+              "background-repeat": "no-repeat",
+              "background-attachment": "fixed",
+              "-webkit-text-fill-color": "transparent",
+              "-webkit-background-clip": "text"
+            });
+        }
+    });
+}
+
+
+getImage();
 load();
 setLocationName();
