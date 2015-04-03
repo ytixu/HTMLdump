cedb115fb52759281b52a5129442bc7616ccbbe4
yitian <yi.t.xu@mail.mcgill.ca>
2015-04-03 12:37:40 -0400

    fix for firefox

--- hacknights/script.js
+++ hacknights/script.js
@@ -13,15 +13,17 @@ function getImage(){
               // "background-blend-mode": "color"
             });
             $(".siteTitle").css({
-              "background": "url(http://bing.com" + data.Paths[0] + ")",
+              // "background": "url(http://bing.com" + data.Paths[0] + ")",
+              "color": "rgba(225,225,225,0.3)",
+              
+              "background": "-webkit-linear-gradient(transparent, transparent), url(http://bing.com" + data.Paths[0] + ") transparent",
+              "background-image": "-o-linear-gradient(transparent, transparent)",
               "background-size": "cover",
               "background-position": "center",
               "background-repeat": "no-repeat",
               "background-attachment": "fixed",
               "-webkit-text-fill-color": "transparent",
-              "-webkit-background-clip": "text",
-              "display": "block"
-              // "background-blend-mode": "color"
+              "-webkit-background-clip": "text"
             });
         }
     });
