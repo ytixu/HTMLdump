a705ef224514289e21f8bb292315836835f124a8
ytixu <yi.t.xu@mail.mcgill.ca>
2015-04-17 00:54:50 -0400

    menu

--- 2015/script/resize.js
+++ 2015/script/resize.js
@@ -1,12 +1,6 @@
 window.onresize = function(){
 	getCanvas();
-	console.log($("#menu").offset().top, $(window).height());
 	if ($("#menu").offset().top < $(window).height()){
-		scrollMenu = window.setTimeout(function(){
-			scrollMenu = null;
-			$("#menu").animate({
-				top: Math.floor($(window).height()).toString() + "px"
-			}, 200)
-		}, 100);
+		moveMenu();
 	}
 }
--- 2015/script/scroll.js
+++ 2015/script/scroll.js
@@ -1,16 +1,45 @@
 var scrollMenu = null;
+var colorChanged = false;
+
+function moveMenu(){
+	if (scrollMenu != null){
+		clearTimeout(scrollMenu);
+	}
+	scrollMenu = window.setTimeout(function(){
+		scrollMenu = null;
+		var value = Math.max(Math.floor($(window).scrollTop()), 
+						  $(window).height()).toString() + "px";
+		$("#menuColor").animate({
+			top: value
+		}, 200);
+		$("#menu").animate({
+			top: value
+		}, 200, function(){
+			if (!colorChanged && Math.abs($("#menu").offset().top - $(window).height()) > 5){
+				colorChanged = true;
+				$("#menuColor").slideDown("fast");
+			}else if (Math.abs($("#menu").offset().top - $(window).height())<5){
+				colorChanged = false;
+				$("#menuColor").slideUp("fast");
+			}
+		});
+	}, 100);
+}
+
+function checkScroll(){
+	if ($(window).scrollTop() > $(window).height() 
+		|| $("#menu").offset().top > $(window).height()){
+		moveMenu();
+	}
+}
 
 window.onscroll = function() {
-	if ($(window).scrollTop() > $(window).height()){
-		if (scrollMenu != null){
-			clearTimeout(scrollMenu);
-		}
-		scrollMenu = window.setTimeout(function(){
-			scrollMenu = null;
-			$("#menu").animate({
-				top: Math.max(Math.floor($(window).scrollTop()), 
-							  Math.floor($(window).height())).toString() + "px"
-			}, 200)
-		}, 100);
-	}
+	checkScroll();
 };
+
+$(function(){
+	$("#menuColor").hide();
+	$("#menuColor").height($("#menu").height());
+	$("#menuColor").width($(window).width());
+	checkScroll();
+})
