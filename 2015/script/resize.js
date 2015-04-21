c800dce8f0318bc6796bff7482298ca7d62e3686
ytixu <yi.t.xu@mail.mcgill.ca>
2015-04-21 00:47:52 -0400

    done?

--- 2015/script/resize.js
+++ 2015/script/resize.js
@@ -10,3 +10,25 @@ window.onresize = function(){
 	$("#menuColor").height($("#menu").height());
 	checkScroll();
 }
+
+
+/////// date for web dev
+
+function getbirthday(){
+	var time = new Date(new Date() - new Date(2014,05,4));
+	var years = time.getFullYear() - new Date(0).getFullYear()-1;
+	console.log(years, years == 0);
+	if (years != 0){
+		$("#bdYear").html(years.toString() + " years");
+	}else{
+		$("#bdYear").html("<br>");
+	}
+	$("#bdMonth").html((time.getMonth()+1).toString() 
+		+ " months");
+	$("#bdDay").html((time.getDate()).toString() 
+		+ " days");
+}
+
+$(function(){
+	getbirthday();
+})
