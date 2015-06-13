613cc76edb78e5fefcac332a178b5559cc9ebd06
yitian <yi.t.xu@mail.mcgill.ca>
2015-06-12 21:54:37 -0400

    recipes

---
+++ misc/foodhacks/script.js
@@ -0,0 +1,7 @@
+var recipes = [
+	{
+		name: "Trottier++ Pasta", 
+		desc: "Homemade fried tomato and onion sause sprinkled with cheese. A lot better than the pasta at Trottier's cafeteria.",
+		img: "trottier-p-p-pasta.jpg"
+	}
+]
