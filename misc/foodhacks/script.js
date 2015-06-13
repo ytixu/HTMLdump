c75c9ff9ef15ce62878173abb2b54a0f0acf2c3b
yitian <yi.t.xu@mail.mcgill.ca>
2015-06-12 22:18:45 -0400

    first hack

--- misc/foodhacks/script.js
+++ misc/foodhacks/script.js
@@ -1,7 +1,14 @@
 var recipes = [
+
 	{
 		name: "Trottier++ Pasta", 
 		desc: "Homemade fried tomato and onion sause sprinkled with cheese. A lot better than the pasta at Trottier's cafeteria.",
 		img: "trottier-p-p-pasta.jpg"
+	},
+	{
+		name: "Hello World", 
+		desc: "Cheese and blackberry on bagel with jelly and chocolate sauce. First food hack :3",
+		img: "helloworld.jpg"
 	}
+
 ]
