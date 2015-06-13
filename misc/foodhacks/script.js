a5f8a35288691a66922dac046f8bc87ec4c2500b
yitian <yi.t.xu@mail.mcgill.ca>
2015-06-12 22:41:38 -0400

    meh

--- misc/foodhacks/script.js
+++ misc/foodhacks/script.js
@@ -6,6 +6,16 @@ var recipes = [
 		img: "trottier-p-p-pasta.jpg"
 	},
 	{
+		name: "Meat Da Salad", 
+		desc: "Fried meat, peas and corn on salad and rice.",
+		img: "meat-the-salad.jpg"
+	},
+	{
+		name: "Coin For Your Stomach", 
+		desc: "Cookies - round and smooth like coins - with cereal, yogurt and chocolate sauce.",
+		img: "coin-for-your-stomach.jpg"
+	},
+	{
 		name: "Hello World", 
 		desc: "Cheese and blackberry on bagel with jelly and chocolate sauce. First food hack :3",
 		img: "helloworld.jpg"
