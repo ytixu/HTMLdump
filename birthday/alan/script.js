8d5aa3b7384468cf0e61870658d73913c7c92282
yitian <yi.t.xu@mail.mcgill.ca>
2015-09-21 23:44:54 -0400

    charts

--- birthday/alan/script.js
+++ birthday/alan/script.js
@@ -1,11 +1,38 @@
-function padListItems(){
-	$childs = $('#side-menu li');
-	$('#side-menu li').each(function(i, e){
-		if(i%2 == 1)
-			$(e).css({'text-align': 'right'});
-	});
+var skillCharts = [
+	{
+		name: "Java",
+		percent: 73,
+		color:'#00CC99'
+	},{
+		name: "Matlab",
+		percent: 42,
+		color:'#FF6699'
+	}
+]
+
+function pieChartInit(){
+	$('.chart').each(function(i, e){
+		console.log($(e));
+		$(e).easyPieChart({
+	        barColor: $(this).data('color'),
+		    trackColor: 'rgba(0,0,0,0.6)',
+	    	size: 200
+	    });
+    });
 }
 
-$(function(){
-	// padListItems();
-}());
+// angular
+
+function viewCtrl($scope) {
+	$scope.skillCharts = skillCharts;
+
+	$scope.refresh = function() {
+		window.setTimeout(function(){
+			//scrollReveal
+			// window.sr = new scrollReveal();
+			// MatJax
+			// MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+			pieChartInit();
+		}, 500);
+	};
+}
